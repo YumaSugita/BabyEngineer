e9bf969e3c4e0a543427158feaa062a404b0ce89
YumaSugita <yum221stg22ral@gmail.com>
2024-01-03 01:24:23 +0900

    Stocks check

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -3,7 +3,12 @@ package pkg;
 public class Main {
 	
 	public static void main(String[] args) {
+		Stocks stocks = new Stocks();
+		
 		System.out.println("Hello, world!");
+		
+		
+		System.out.println(stocks.taste[1]);
 	}
 
 }
--- src/pkg/OrderInfo.java
+++ src/pkg/OrderInfo.java
@@ -1,5 +1,23 @@
 package pkg;
 
 public class OrderInfo {
+	public static void getTaste() {
+		
+	}
+	public static void getDensity() {
+		
+	}
+	public static void getTypeNoodle() {
+		
+	}
+	public static void getHardnessNoodle() {
+		
+	}
+	public static void getAmountNoodle() {
+		
+	}
+	public static void getTopping() {
+		
+	}
 	
 }
