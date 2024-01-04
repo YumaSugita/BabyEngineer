03ce80c24f811404dab50d54e666e7f78d226ad9
YumaSugita <yum221stg22ral@gmail.com>
2024-01-04 22:52:52 +0900

    reflesh folder

--- src/pkg/OrderInfo.java
+++ src/pkg/OrderInfo.java
@@ -1,7 +1,14 @@
 package pkg;
 
+import java.util.Scanner;
+
 public class OrderInfo {
+	Scanner scan = new Scanner(System.in);
+	Stocks stocks = new Stocks();
+	
 	public static void getTaste() {
+		System.out.println("スープを選んでください。 1.醤油 2.塩 3.味噌 4.豚骨 5.魚介系");
+//		String str = scan.nextLine();
 		
 	}
 	public static void getDensity() {
